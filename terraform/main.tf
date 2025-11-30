2f247c20368fe91bc6a4b85b8e3270688f43bef6
Akhil Ankam <akhilankam4747@gmail.com>
2025-11-30 15:20:27 +0530

    Made changes to providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.region
-}
-
 module "vpc" {
   source = "git::https://github.com/akhilankam/infra-modules.git//vpc"
 
@@ -10,3 +6,33 @@ module "vpc" {
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
 }
+
+module "iam" {
+  source = "git::https://github.com/<your-user>/infra-modules.git//iam"
+
+  cluster_name = "my-eks-cluster"
+}
+
+module "eks" {
+  source = "git::https://github.com/<your-user>/infra-modules.git//eks"
+
+  cluster_name           = "my-eks-cluster"
+  eks_cluster_role_arn   = module.iam.eks_cluster_role_arn
+  eks_node_role_arn      = module.iam.eks_node_role_arn
+  private_subnets        = module.vpc.private_subnets
+}
+
+module "rds" {
+  source = "git::https://github.com/<your-user>/infra-modules.git//rds"
+
+  vpc_security_group_ids = [module.vpc.database_sg_id]
+  subnet_ids             = module.vpc.private_subnets
+}
+
+module "alb_ingress" {
+  source = "git::https://github.com/<your-user>/infra-modules.git//alb"
+
+  cluster_name              = module.eks.cluster_name
+  cluster_oidc_provider_arn = module.eks.oidc_provider_arn
+}
+
---
+++ terraform/provider.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+     random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
